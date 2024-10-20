6a0217dd637ebbfb888fee9a588193eb01815c3a
Clément Malige <clement@rakam.ai>
2024-10-20 17:22:26 +0200

    health check endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 from starlette.templating import Jinja2Templates
 
 app = FastAPI()
@@ -11,3 +12,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("homepage.html", context={"request": request})
+
+
+@app.get("/health", response_class=JSONResponse)
+async def health_check():
+    return {"status": "healthy"}
