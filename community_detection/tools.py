f14f4b0a4eece0ef208a7041d57a18a0e5777974
Clément Malige <clement@rakam.ai>
2024-11-04 11:46:31 +0100

    update tools

--- community_detection/tools.py
+++ community_detection/tools.py
@@ -1,10 +1,12 @@
 import gzip
+import io
+from typing import Any
 
 import networkx as nx
 
 
 def read_edges_with_ports_to_graph(edges_file: str) -> nx.Graph:
-    G = nx.Graph()
+    graph = nx.Graph()
     with gzip.open(edges_file, mode="rt") as fopen:
         for line in fopen:
             if line.startswith("#"):  # skip comment lines
@@ -13,5 +15,22 @@ def read_edges_with_ports_to_graph(edges_file: str) -> nx.Graph:
             if len(parts) < 3:
                 continue
             node1, node2 = parts[1], parts[2]
-            G.add_edge(node1, node2)
-    return G
+            graph.add_edge(node1, node2)
+    return graph
+
+
+def save_plot(plot: Any) -> io.BytesIO:
+    """
+    Saves a Matplotlib plot to an in-memory BytesIO buffer as a PNG image.
+
+    Args:
+        plot (Any): The Matplotlib plot object (usually matplotlib.pyplot or a Figure object).
+
+    Returns:
+        io.BytesIO: A buffer containing the PNG image data of the plot.
+    """
+    buf = io.BytesIO()
+    plot.savefig(buf, format="png")
+    buf.seek(0)
+    plot.close()
+    return buf
