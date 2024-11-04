3c575d970be4262e8009289e774d359ef6ef2d32
Clément Malige <clement@rakam.ai>
2024-11-04 10:50:55 +0100

    endpoint tags

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse, tags=["Base"])
 async def root(request: Request):
     return templates.TemplateResponse("homepage.html", context={"request": request})
 
 
-@app.get("/health", response_class=JSONResponse)
+@app.get("/health", response_class=JSONResponse, tags=["Base"])
 async def health_check():
     return {"status": "healthy"}
 
 
-@app.get("/community", response_class=JSONResponse)
+@app.get("/community", response_class=JSONResponse, tags=["Community"])
 async def run_community():
     return {"status": "completed"}
