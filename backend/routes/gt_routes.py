97b509aad35d5c8117c489067e711e66ac11c29f
Clément Malige <clement@rakam.ai>
2024-11-25 20:58:39 +0100

    new algo/viz

--- backend/routes/gt_routes.py
+++ backend/routes/gt_routes.py
@@ -11,15 +11,15 @@ gt_router = APIRouter(prefix="/gt", tags=["Ground Truth"])
 
 
 @gt_router.get("/", response_class=JSONResponse)
-async def ground_truth(file_size: FileSize = FileSize.SMALL):
+async def ground_truth(file_size: FileSize = FileSize.SMALL_2D):
     return ground_truth_service(file_size=file_size)
 
 
 @gt_router.get("/stats", response_class=JSONResponse)
-async def ground_truth_statistics(file_size: FileSize = FileSize.SMALL):
+async def ground_truth_statistics(file_size: FileSize = FileSize.SMALL_2D):
     return ground_truth_statistics_service(file_size=file_size)
 
 
 @gt_router.get("/histogram", response_class=HTMLResponse)
-async def ground_truth_histogram(file_size: FileSize = FileSize.SMALL):
+async def ground_truth_histogram(file_size: FileSize = FileSize.SMALL_2D):
     return display_histogram_service(file_size=file_size)
--- backend/tools/path_selecter.py
+++ backend/tools/path_selecter.py
@@ -1,11 +1,28 @@
 from backend.tools.custom_enums import FileSize
 
-small_path = "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_2days_feb10thruFeb11_all_49sensors.csv.txt.gz"
+small_2d_path = "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_2days_feb10thruFeb11_all_49sensors.csv.txt.gz"
+small_4d_path = "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_4days_feb10thruFeb13_all_49sensors.csv.txt.gz"
+small_12h_path = "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_12hrs_feb10_all_49sensors.csv.txt.gz"
 large_path = "Cisco_22_networks/dir_20_graphs/dir_day1/out1_1.txt.gz"
 test = "Cisco_22_networks/dir_g21_small_workload_with_gt/grouping.gt.txt"
+
+
+rnd_small = [
+    "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_2days_feb10thruFeb11_all_49sensors.csv.txt.gz",
+    "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_4days_feb10thruFeb13_all_49sensors.csv.txt.gz",
+    "Cisco_22_networks/dir_g21_small_workload_with_gt/dir_no_packets_etc/edges_12hrs_feb10_all_49sensors.csv.txt.gz",
+]
 
 
 def path_selecter(file_size: FileSize) -> str:
     if file_size == FileSize.LARGE:
         return large_path
-    return small_path
+    if file_size == FileSize.SMALL_12H:
+        return small_12h_path
+    if file_size == FileSize.SMALL_2D:
+        return small_2d_path
+    if file_size == FileSize.SMALL_4D:
+        return small_4d_path
+    if file_size == FileSize.TEST:
+        return test
+    return small_2d_path
