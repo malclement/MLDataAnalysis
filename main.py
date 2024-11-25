47f440fe001681bbed8864162187482101793a49
Clément Malige <clement@rakam.ai>
2024-11-25 13:31:11 +0100

    add routing

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
-from fastapi import Query
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 from starlette.templating import Jinja2Templates
 
+from backend.routes.gt_routes import gt_router
 from backend.services.community_services import run_community_service
-from backend.services.gt_services import ground_truth_service
-from backend.services.gt_services import ground_truth_visualization_service
-from backend.services.gt_statistic_services import display_histogram_service
-from backend.services.gt_statistic_services import ground_truth_statistics_service
 from backend.tools.custom_enums import CommunityAlgorithm
 from backend.tools.custom_enums import FileSize
 
-app = FastAPI()
+app = FastAPI(
+    title="Cisco Analysis - API",
+    description="API to trigger cisco analysis mechanism",
+    version="1.0.0",
+    redoc_url="/docs",
+)
 
 templates = Jinja2Templates(directory="templates")
+
+app.include_router(gt_router)
 
 
 @app.get("/", response_class=HTMLResponse, tags=["Base"])
@@ -33,26 +36,3 @@ async def run_community(
     algorithm: CommunityAlgorithm, file_size: FileSize = FileSize.SMALL
 ):
     return run_community_service(algorithm=algorithm, file_size=file_size)
-
-
-@app.get("/gt", response_class=JSONResponse, tags=["Ground Truth"])
-async def ground_truth(file_size: FileSize = FileSize.SMALL):
-    return ground_truth_service(file_size=file_size)
-
-
-@app.get("/gt/visualize", response_class=HTMLResponse, tags=["Ground Truth"])
-async def ground_truth_visualization(
-    file_size: FileSize = FileSize.SMALL,
-    save: bool = Query(False, description="Save as img"),
-):
-    return ground_truth_visualization_service(file_size=file_size)
-
-
-@app.get("/gt/stats", response_class=JSONResponse, tags=["Ground Truth"])
-async def ground_truth_statistics(file_size: FileSize = FileSize.SMALL):
-    return ground_truth_statistics_service(file_size=file_size)
-
-
-@app.get("/gt/histogram", response_class=HTMLResponse, tags=["Ground Truth"])
-async def ground_truth_histogram(file_size: FileSize = FileSize.SMALL):
-    return display_histogram_service(file_size=file_size)
